dcf528e7ce137e454b0e7c9dee40a975ce8f5148
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-02-20 23:44:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ app.get('/documentation', (req, res) => {
 //Requests for Mongoose
 
 //GET list of all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
