5be28306a7742bb5d7f75bda0d7e1bee428ab99e
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-02-21 00:35:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,20 +34,7 @@ const passport = require('passport');
 require('./passport');
 
 const cors = require('cors');
-//app.use(cors());
-
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 
 const { check, validationResult } = require('express-validator');
